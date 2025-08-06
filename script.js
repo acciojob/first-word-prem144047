a1e9df1c0f6924bf5b686d9ebbb37db7ac9ac343
Prem Kumar <prem144047@gmail.com>
2025-08-06 22:50:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
   // your code here
+	 if (s === "") {
+        return "";
+    }
+
+    // Step 2: Find the index of the first space
+    const spaceIndex = s.indexOf(" ");
+
+    // Step 3: Extract the first word
+    if (spaceIndex !== -1) {
+        return s.substring(0, spaceIndex);
+    } else {
+        return s; // No space found, return the whole string
+    }
 }
 
 // Do not change the code below
